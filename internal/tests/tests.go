de02130e2730e3abb725366e8b9a20b005aed907
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-02-05 20:27:25 +0330

    build: update tests and manifests
    issue #14

---
+++ internal/tests/tests.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	scapiv1alpha3 "github.com/operator-framework/api/pkg/apis/scorecard/v1alpha3"
+	apimanifests "github.com/operator-framework/api/pkg/manifests"
+)
+
+const (
+	CustomTest1Name = "customtest1"
+)
+
+// CustomTest1
+func CustomTest1(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
+	r := scapiv1alpha3.TestResult{}
+	r.Name = CustomTest1Name
+	r.State = scapiv1alpha3.PassState
+	r.Errors = make([]string, 0)
+	r.Suggestions = make([]string, 0)
+
+	// Implement relevant custom test logic here
+
+	return wrapResult(r)
+}
